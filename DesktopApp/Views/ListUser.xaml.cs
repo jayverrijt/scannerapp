679a6cc060910cf153c844f91c9e10073a15b71a
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2023-09-28 15:52:31 +0200

    [28-sept-23] User overview

--- DesktopApp/Views/ListUser.xaml.cs
+++ DesktopApp/Views/ListUser.xaml.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using DesktopApp.Classes;
 
-namespace DesktopApp.Views;
-
-public partial class ListUser : ContentPage
+namespace DesktopApp.Views
 {
-    public ListUser()
+    public partial class ListUser : ContentPage
     {
-        InitializeComponent();
+        public ListUser()
+        {
+            InitializeComponent();
+            CollectUsers cu = new CollectUsers();
+            var Adminlist = cu.getAdminUsers();
+            foreach(var Adminitem in Adminlist)
+            {
+                MyAdminListView.ItemsSource = Adminlist;
+            }
+
+            var Clientlist = cu.getClientUsers();
+            foreach (var Clientitem in Clientlist)
+            {
+                MyClientListView.ItemsSource = Clientlist;
+            }
+
+
+        }
     }
 }
